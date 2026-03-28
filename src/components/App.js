1920109ee85bfae80addd4e98188d7fa3c45ad0a
Vinayak260 <vinayakchavan271@gmail.com>
2026-03-28 14:18:48 +0530

    vinayak

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,52 @@
+import React, { useRef, useState } from "react";
 
-import React from "react";
-import './../styles/App.css';
+function App() {
+  const totalItems = 1000;
+  const itemHeight = 50;
+  const visibleCount = 10;
 
-const App = () => {
+  const scrollRef = useRef(null);
+
+  const [startIndex, setStartIndex] = useState(0);
+
+  const items = Array.from({ length: totalItems }, (_, i) => ({
+    title: `Item ${i}`,
+    desc: "Lorem ipsum dolor sit amet."
+  }));
+
+  const handleScroll = () => {
+    const scrollTop = scrollRef.current.scrollTop;
+    const newStart = Math.floor(scrollTop / itemHeight);
+    setStartIndex(newStart);
+  };
+
+  const visibleItems = items.slice(startIndex, startIndex + visibleCount);
+
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div
+      ref={scrollRef}
+      onScroll={handleScroll}
+      style={{ height: "500px", overflow: "auto" }}
+    >
+      <div style={{ height: `${startIndex * itemHeight}px` }} />
+
+      {visibleItems.map((item, index) => (
+        <div
+          key={startIndex + index}
+          style={{ height: `${itemHeight}px` }}
+        >
+          <h2>{item.title}</h2>
+          <p>{item.desc}</p>
+        </div>
+      ))}
+
+      <div
+        style={{
+          height: `${(totalItems - (startIndex + visibleCount)) * itemHeight}px`
+        }}
+      />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
